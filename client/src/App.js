6c2624982dae94b4fdac1559eca8eda26048be9c
meghnadutta02 <meghnadutta02@gmail.com>
2023-09-15 21:26:18 +0530

    refresh token functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -8,33 +8,45 @@ import VerifyPage from "./page/Verify";
 import Vote from "./page/Vote.jsx";
 import Guide from "./page/Guide.jsx";
 import Profile from "./page/Profile.jsx";
+import axios from "axios";
 import ProtectedRoutes from "./component/ProtectedRoutes.js";
 const App = () => {
+  const refreshAccessToken = async () => {
+    try {
+      const response = await axios.get("/api/users/refresh-token");
+      if (response.status === 403 || response.status === 401) return;
+    } catch (error) {
+      console.error(error);
+    }
+  };
  
+  const refreshInterval = 60*1000*19;
+
+  setInterval(refreshAccessToken, refreshInterval);
+
   return (
     <Routes>
-     
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/verify" element={<VerifyPage />} />
-      <Route path="/vote" element={<Vote />} />
+     
       <Route path="/profile" element={<Profile />} />
       <Route path="/guide" element={<Guide />} />
 
       {/* <Route path="/verify" element={<VerifyPage />} /> */}
 
       {/* user protected routes */}
-        {/* <Route element={<ProtectedRoutes admin={false} superAdmin={false} />}> 
-     
+      {/* <Route element={<ProtectedRoutes admin={false} superAdmin={false} />}> 
+      
       </Route>  */}
 
-       {/* admin protected routes */}
-      {/* <Route element={<ProtectedRoutes admin={true} superAdmin={false} />}>
+      {/* admin protected routes */}
+      <Route element={<ProtectedRoutes admin={true} superAdmin={false} />}>
+      <Route path="/vote" element={<Vote />} />
+      </Route>
 
-      </Route> */}
-
-       {/* superadmin protected routes */}
+      {/* superadmin protected routes */}
       {/* <Route element={<ProtectedRoutes superAdmin={true} admin={false} />}>
       
       </Route> */}
